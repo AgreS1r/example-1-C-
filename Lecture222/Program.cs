75f153242e2221c60c7a0246959c6f54bb1e2668
danil <faceitplay2004@mail.ru>
2023-03-25 21:27:37 +0300

    задания по лекции 2, изучение С#

---
+++ Lecture222/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {51,25,13,4,25,68,217,48,41};
+
+int n = array.Length;
+int find = 25;
+
+int index = 0;
+
+while (index < n){
+    if (array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
